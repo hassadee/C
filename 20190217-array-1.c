e92f3bdb09b4169e1c300f27b145d06d81f4f32f
Hassadee Pimsuwan <h.pimsuwan@gmail.com>
2019-02-17 11:11:09 +0700

    [+] SP: Add array example;

---
+++ 20190217-array-1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char msg[]="PEDYANG MK";
+    int len,i;
+    
+    len=strlen(msg);
+    for(i=0;i<len;i++)
+        printf("%d : %c\n", i, msg[i]);
+
+    return 0;
+}
